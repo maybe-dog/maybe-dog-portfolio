3c2eab9ca9b183607a4ff3fae69917a3bf697af3
maybe_dog <sa990130@gmail.com>
2025-03-20 02:09:54 +0900

    feat: deleted stories

--- src/components/elements/Timeline/SimpleTimeline.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-import SimpleTimeline from './SimpleTimeline';
-
-export default { component: SimpleTimeline } as Meta<typeof SimpleTimeline>;
-
-export const Default: StoryObj<typeof SimpleTimeline> = {
-  args: {},
-};
